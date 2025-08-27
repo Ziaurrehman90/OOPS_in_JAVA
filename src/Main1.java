627e6ecc1cceb45eb866d7a3d4f7cda4d99bafe7
I578281 <ziaurrehman@sap.com>
2025-08-27 14:30:27 +0530

    class & objects

---
+++ src/Main1.java
@@ -0,0 +1,7 @@
+public class Main1 {
+//    class : its like the blueprint
+//     for eg fruit could be a class and its ojects are apple,bananan, pineapple etc.
+//    for eg. car could be classs and its obbjects are bmw, mercedes, aulto etc.
+//     so basocally objects are instances of  the class.
+
+}
