dc3adbf47f80924d8c2d1af1a16d59c33f41a54a
I578281 <ziaurrehman@sap.com>
2025-08-27 14:31:56 +0530

    class & objects

--- src/Main1.java
+++ src/Main1.java
@@ -3,5 +3,8 @@ public class Main1 {
 //     for eg fruit could be a class and its ojects are apple,bananan, pineapple etc.
 //    for eg. car could be classs and its obbjects are bmw, mercedes, aulto etc.
 //     so basocally objects are instances of  the class.
+    public static void main(String[] args) {
+        System.out.println("Learning of classes and objects");
+    }
 
 }
