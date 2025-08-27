a8f0edd2b2415dd64b4d27df1b33e2212c4efebd
I578281 <ziaurrehman@sap.com>
2025-08-27 14:44:56 +0530

    new commit

--- src/Main1.java
+++ src/Main1.java
@@ -3,11 +3,17 @@ public class Main1 {
 //     for eg fruit could be a class and its ojects are apple,bananan, pineapple etc.
 //    for eg. car could be classs and its obbjects are bmw, mercedes, aulto etc.
 //     so basocally objects are instances of  the class.
+//    Everything in java is associated with classes and objects
+//    objects has some properties callled attributes and some methods or finctuons
+//    foreg. a volvo car can ve a object of class CAR
+//     it has properties like : white color, powerful engine etc.
+//    and it also do have some methods or functions like : brake, driving etc.
+//    this could be the concept of classes and objects .
     public static void main(String[] args) {
         System.out.println("Learning of classes and objects");
         System.out.println("basically class is a template for the object and object is the instance of the clasass");
         System.out.println("begnning with class and objects ");
-        
+
     }
 
 }
