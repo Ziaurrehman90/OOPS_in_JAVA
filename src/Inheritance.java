00a67eab24bbaa220695b8302ad06887a95e2059
I578281 <ziaurrehman@sap.com>
2025-08-29 13:59:01 +0530

    inheritance

---
+++ src/Inheritance.java
@@ -0,0 +1,15 @@
+//syntax of writting the inheritance
+//use extend keyword
+class Parent{
+
+}
+class Child extends Parent{
+
+}
+public class Inheritance {
+//    Inheritance is one of the four pillars of OOP (Object-Oriented Programming).
+//    It allows one class (called child / subclass / derived class) to inherit properties and methods from another class (called parent / superclass / base class).
+//ava supports single inheritance, multilevel inheritance, hierarchical inheritance, and hybrid inheritance (through interfaces).
+//Multiple inheritance with classes is NOT supported (to avoid ambiguity, a.k.a. “Diamond Problem”), but it is possible with interfaces.
+
+}
